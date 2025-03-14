37f225511e5bbd0624889f02e61ff10a3ac116a8
Kevin Sie <kevinsie223@gmail.com>
2025-03-14 17:52:12 +0800

    Initial Commit

---
+++ ClockBurn/ClockBurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockBurnApp.swift
+//  ClockBurn
+//
+//  Created by Kevin on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct ClockBurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockBurnTests/ClockBurnTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClockBurnTests.swift
+//  ClockBurnTests
+//
+//  Created by Kevin on 2025/3/14.
+//
+
+import Testing
+@testable import ClockBurn
+
+struct ClockBurnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
